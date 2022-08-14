21903123f73e75ed543607e61a7cbfe73dabad09
Ismael Salamea <79610522+ismasalamea@users.noreply.github.com>
2022-08-13 19:46:05 -0500

    Update map.js

--- js/map.js
+++ js/map.js
@@ -1,10 +1,37 @@
 //Definimos la coordenada central y el zoom
-var map = L.map('map').setView([-2.8866116,-79.1509034], 11);
+let config = {
+  minZoom: 7,
+  maxZoom: 18,
+  // https://github.com/Leaflet/Leaflet.fullscreen
+  fullscreenControl: true,
+};
+// magnification with which the map will start
+const zoom = 11;
+// co-ordinates
+const lat = -2.8866116;
+const lng = -79.1509034;
+
+// calling map
+const map = L.map('map', config).setView([lat, lng], zoom);
+
 L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
     maxZoom: 25
 }).addTo(map);
 L.control.scale().addTo(map);
+
+//map.on('click', function(e) {
+//    alert(e.latlng);
+//} );
+
+function clickZoom(e) {
+// con zoom
+//  map.setView(e.target.getLatLng(), zoom);
+//sin zoom
+  map.setView(e.target.getLatLng());
+  // pantTo version
+  // map.panTo(e.target.getLatLng());
+}
 
 // geoJSON para agregar capas desde formato JSON 
 
@@ -23,7 +50,8 @@ function popup(feature, layer) {
         <b>TELEFONO:</b> ${feature.properties.TELEFONO}<br/> 
         <b>CONTACTO:</b> ${feature.properties.CONTACTO}<br/> 
         <b>CORREO:</b> ${feature.properties.CORREO}<br/></p>`
-        ); 
+        );
+	layer.on("click", clickZoom);
     } 
 }
 ///Crear un tooltip (label) de la entidades de la capa geoJSON
@@ -69,4 +97,60 @@ function tooltip(feature, layer) {
             });    
                 return L.marker(latlng, {icon: normal});     
     };       
-}*/;
+};*/
+
+
+/// BOTON HOME
+const icono ="<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 32 32'><path d='M32 18.451L16 6.031 0 18.451v-5.064L16 .967l16 12.42zM28 18v12h-8v-8h-8v8H4V18l12-9z' /></svg>";
+
+// create custom button
+const customControl = L.Control.extend({
+  // button position
+  options: {
+    position: "topleft",
+  },
+
+  // method
+  onAdd: function (map) {
+    console.log(map.getCenter());
+    // create button
+    const btn = L.DomUtil.create("button");
+    btn.title = "back to home";
+    btn.innerHTML = icono;
+    btn.className += "leaflet-bar back-to-home hidden";
+    return btn;
+  },
+});
+
+// adding new button to map controll
+map.addControl(new customControl());
+
+// on drag end
+map.on("moveend", getCenterOfMap);
+
+const buttonBackToHome = document.querySelector(".back-to-home");
+
+function getCenterOfMap() {
+  buttonBackToHome.classList.remove("hidden");
+
+  buttonBackToHome.addEventListener("click", () => {
+    map.flyTo([lat, lng], zoom);
+  });
+
+  map.on("moveend", () => {
+    const { lat: latCenter, lng: lngCenter } = map.getCenter();
+
+    const latC = latCenter.toFixed(3) * 1;
+    const lngC = lngCenter.toFixed(3) * 1;
+
+    const defaultCoordinate = [+lat.toFixed(3), +lng.toFixed(3)];
+
+    const centerCoordinate = [latC, lngC];
+
+    if (compareToArrays(centerCoordinate, defaultCoordinate)) {
+      buttonBackToHome.classList.add("hidden");
+    }
+  });
+}
+
+const compareToArrays = (a, b) => JSON.stringify(a) === JSON.stringify(b);
